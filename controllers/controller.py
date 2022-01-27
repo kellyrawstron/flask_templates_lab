42b07b5ff0ff7ddd8802c4b1c24eb08fe11752f6
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-01-27 15:07:36 +0000

    MVP almost done

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,3 +3,9 @@ from app import app
 from models.event_list import events
 from models.event import Event
 
+
+@app.route('/events')
+def index():
+    return render_template('index.html', title='Home', events=events)
+
+@app.route('/events')
